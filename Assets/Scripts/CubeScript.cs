6b550721bcc78c86dd86a07568dd87aadb3d5a46
AnotherCat <artmuk.for.game@gmail.com>
2016-11-11 21:08:16 +0700

    i hate shotgun

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class CubeScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
-
     public void ToggleVRMode()
     {
         GvrViewer.Instance.VRModeEnabled = !GvrViewer.Instance.VRModeEnabled;
